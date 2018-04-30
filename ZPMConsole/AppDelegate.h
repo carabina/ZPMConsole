0d04df5b4e404ca32cc931d2e686ddd99a8d9462
MMR无与伦比 <truend86@qq.com>
2018-04-30 23:20:53 +0800

    提交初始工程

---
+++ ZPMConsole/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZPMConsole
+//
+//  Created by 刘钊 on 2018/4/30.
+//  Copyright © 2018年 Liu Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZPMConsole/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZPMConsole
+//
+//  Created by 刘钊 on 2018/4/30.
+//  Copyright © 2018年 Liu Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
